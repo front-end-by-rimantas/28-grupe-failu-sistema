740cc50a947b543c0ded9a9a23802cdfaa4ad949
Rimantas <belauzas@yahoo.com>
2021-07-26 18:49:57 +0300

    File system: create

--- index.js
+++ index.js
@@ -8,16 +8,25 @@ _data.read('users', 'petras', (err, data) => {
 
     const obj = JSON.parse(data);
     console.log(obj);
-    console.log(obj.name);
-    console.log(obj.age);
-    console.log(obj.car);
 })
 
-// _data.read('users', 'maryte', (err, data) => {
-//     if (err || !data) {
-//         console.log('Nepavyko perskaityti failo...');
-//         return false;
-//     }
 
-//     console.log(data);
-// })
+const pazymiai = [10, 2, 8, 4, 6];
+_data.create('marks', 'kazys', pazymiai, (err) => {
+    if (err) {
+        console.log(err);
+        return false;
+    }
+
+    console.log('Failas sekmingai sukurtas!');
+
+    _data.read('marks', 'kazys', (err, data) => {
+        if (err || !data) {
+            console.log('Nepavyko perskaityti Kazio failo...');
+            return false;
+        }
+
+        const obj = JSON.parse(data);
+        console.log(obj);
+    })
+});
