8669aefb356e03749ed324e33fd78a88904e7ce0
Rimantas <belauzas@yahoo.com>
2021-07-27 18:26:19 +0300

    File system: update

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
 const _data = require('./lib/data');
 
-_data.read('users', 'petras', (err, data) => {
-    if (err || !data) {
-        console.log('Nepavyko perskaityti failo...');
-        return false;
-    }
+// _data.read('users', 'petras', (err, data) => {
+//     if (err || !data) {
+//         console.log('Nepavyko perskaityti failo...');
+//         return false;
+//     }
 
-    const obj = JSON.parse(data);
-    console.log(obj);
-})
+//     const obj = JSON.parse(data);
+//     console.log(obj);
+// })
 
 
-const pazymiai = [10, 2, 8, 4, 6];
-_data.create('marks', 'kazys', pazymiai, (err) => {
+// const pazymiai = [10, 2, 8, 4, 6];
+// _data.create('marks', 'kazys', pazymiai, (err) => {
+//     if (err) {
+//         console.log(err);
+//         return false;
+//     }
+
+//     console.log('Failas sekmingai sukurtas!');
+
+//     _data.read('marks', 'kazys', (err, data) => {
+//         if (err || !data) {
+//             console.log('Nepavyko perskaityti Kazio failo...');
+//             return false;
+//         }
+
+//         const obj = JSON.parse(data);
+//         console.log(obj);
+//     })
+// });
+
+const petras = {
+    name: 'Petras',
+    age: 99,
+    car: 'Audi',
+    color: 'red'
+}
+_data.update('users', 'petras', petras, (err) => {
     if (err) {
         console.log(err);
         return false;
     }
 
-    console.log('Failas sekmingai sukurtas!');
+    console.log('Petro duomenys atnaujinti sekmingai.');
 
-    _data.read('marks', 'kazys', (err, data) => {
+    _data.read('users', 'petras', (err, data) => {
         if (err || !data) {
-            console.log('Nepavyko perskaityti Kazio failo...');
+            console.log('Nepavyko perskaityti Petro failo...');
             return false;
         }
 
